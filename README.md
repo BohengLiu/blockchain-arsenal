c3fe078a0d08ce72743061590a1721439380ce82
robot-l <robot.l@binance.com>
2021-11-28 23:44:51 +0800

    feat: first demo

---
+++ README.md
@@ -0,0 +1,17 @@
+# A Super Engine for web3 developer
+
+# How to start
+1. install rush
+```
+yarn global add @microsoft/rush
+npm i -g @microsoft/rush
+```
+2. install pnpm
+```
+yarn global add pnpm
+npm i -g pnpm
+```
+3. install dependencies
+```
+rush update
+```
--- apps/landing-demo/src/App.tsx
+++ apps/landing-demo/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Thing } from "@shared/components";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Thing />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
